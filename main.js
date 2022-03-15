97a85a12382930f32d43abf4096bec2343feb77d
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2022-03-15 19:51:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
      canvas = createCanvas(280,280);
      canvas.center();
      background("white");
-     canvas.mouseReleased(classifyCanvas());
+     canvas.mouseReleased(classifyCanvas);
      synth = window.speechSynthesis;
  }
 
